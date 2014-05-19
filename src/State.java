5217e972f8f99496b99bfddba1faa0905a45a5d4
welbert <welberts@gmail.com>
2014-05-19 19:09:28 -0300

    Update State.java

--- src/State.java
+++ src/State.java
@@ -1,4 +1,12 @@
+package states;
+import java.util.ArrayList;
+
 
 public class State {
-
+	
+	public ArrayList<Transition> transitions;
+	
+	public State(){
+		transitions = new ArrayList<Transition>();
+	}
 }
